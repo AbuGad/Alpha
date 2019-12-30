507d8030ba8eeb321e23ae3f85cad06917eff7c6
Gad Test <Mgad756@gmail.com>
2019-12-30 12:58:17 -0500

    create article from UI

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,18 +2,21 @@ class ArticlesController < ApplicationController
 
 	def new
 	 @article = Article.new
-
 	end
 
 	def create
-		#render plain: params[:article].inspect
 		@article = Article.new(article_params)
-		@article.save
-	end
-
-	private
-	def article_params
-		params.require(:article).permit(:title, :description)
-		
+		if @article.save
+			flash[:noties] = "Article Was Successfully Created"
+			redirect_to article_path(@article)
+		else
+			render 'new'
 	end
 end
+
+	private
+
+	def article_params
+		params.require(:article).permit(:title, :description)
+		end
+	end
