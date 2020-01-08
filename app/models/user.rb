cc18e6df80ad4b2fb6000691a8678265636bbb77
Gad Test <Mgad756@gmail.com>
2020-01-08 15:10:53 -0500

    add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
-
+	validates :username, uniqueness: { case_sensitive: false },
+	 			presence: true, length: {minimum:3 , maximum: 300}
+	validates :email, uniqueness: { case_sensitive: false}, presence: true ,
+				 length: {minimun:2 , maximum:300}, :email_format => {:message => " is not looking good"} 			
 end
