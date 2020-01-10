0256cb5da6319e5d0182289be9af3c3c2bb52b0d
Gad Test <Mgad756@gmail.com>
2020-01-10 11:40:16 -0500

    update users from UI

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,20 @@ class UsersController<ApplicationController
 		else
 			render 'new'
 		end
-		end
-		
+	end
+
+	def edit
+		@user = User.find(params[:id])			
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+			flash[:success] = 'Your Account Was Updated Successfully'
+			redirect_to articles_path
+			else
+				render 'edit'
+			end	
 	end
 
 	private
@@ -21,3 +33,4 @@ class UsersController<ApplicationController
 	def user_params
 		params.require(:user).permit(:username, :email, :password)
 	end
+end
