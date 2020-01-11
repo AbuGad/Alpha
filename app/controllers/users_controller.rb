e93d76e0d42b3eb78155c8b218948cb469b6c4c4
Gad Test <Mgad756@gmail.com>
2020-01-11 13:26:49 -0500

    img & listing User Article

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,32 +1,37 @@
 class UsersController<ApplicationController
 	
-	def new
-		@user = User.new
-	end
+ def new
+	@user = User.new
+ end
 
-	def create
-		@user = User.new(user_params)
-		if @user.save
-			flash[:success] = "welcome #{@user.username}"
-			redirect_to articles_path
-		else
-			render 'new'
-		end
-	end
+ def create
+	@user = User.new(user_params)
+  if @user.save
+	flash[:success] = "welcome #{@user.username}"
+	redirect_to articles_path
+ else
+	render 'new'
+ end
+end
 
-	def edit
-		@user = User.find(params[:id])			
-	end
+ def edit
+	@user = User.find(params[:id])			
+end
 
-	def update
-		@user = User.find(params[:id])
-		if @user.update(user_params)
-			flash[:success] = 'Your Account Was Updated Successfully'
-			redirect_to articles_path
-			else
-				render 'edit'
-			end	
-	end
+ def update
+	@user = User.find(params[:id])
+ if @user.update(user_params)
+    flash[:success] = 'Your Account Was Updated Successfully'
+    redirect_to articles_path
+ else
+	render 'edit'
+ end	
+end
+
+def show
+	@user = User.find(params[:id])
+	
+end
 
 	private
 
