7d5bcd6b6c4f19e90235b827de88e9a265405f86
Gad Test <Mgad756@gmail.com>
2020-01-11 17:12:47 -0500

    signup and login user from ui

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController<ApplicationController
-	
+	before_action :set_user, only:[:edit,:update,:show]
  def new
 	@user = User.new
  end
@@ -15,11 +15,10 @@ class UsersController<ApplicationController
 end
 
  def edit
-	@user = User.find(params[:id])			
+			
 end
 
  def update
-	@user = User.find(params[:id])
  if @user.update(user_params)
     flash[:success] = 'Your Account Was Updated Successfully'
     redirect_to articles_path
@@ -29,7 +28,6 @@ end
 end
 
 def show
-	@user = User.find(params[:id])
 	@user_articles = @user.articles.paginate(page:params[:page],per_page: 3)
 	
 end
@@ -44,4 +42,9 @@ end
 	def user_params
 		params.require(:user).permit(:username, :email, :password)
 	end
+end
+
+def set_user
+	@user = User.find(params[:id])
+	
 end
