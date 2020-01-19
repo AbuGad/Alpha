25be22cb10e215d05a3a38663f9b3984fb7fc6fc
Gad Test <Mgad756@gmail.com>
2020-01-19 13:10:33 -0500

    update all categories viwe

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,9 @@
 class CategoriesController < ApplicationController
+	
 	before_action :require_admin, except: [:index, :show]
+	
 	def index
-		@categories = Category.paginate(page: params[:page], per_page: 5)
+		@categories = Category.paginate(page: params[:page], per_page: 4)
 	end
 
 	def new
@@ -19,7 +21,8 @@ class CategoriesController < ApplicationController
 	end
 
 	def show
-		
+		@category = Category.find(params[:id])
+		@category_articles = @category.articles.paginate(page: params[:page], per_page: 3)
 	end
 
 	def edit
